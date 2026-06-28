fefd73f6b86023bab2e9d32795d0bd456d392b97
Alexius79 <alexhi031079@gmail.com>
2026-06-28 10:18:15 +0200

    Startscreen mit Brain-Hintergrund

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -5,8 +5,22 @@ type AppLayoutProps = {
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <main className="h-dvh overflow-hidden bg-black text-white">
-      <div className="relative mx-auto h-dvh max-w-[430px] overflow-hidden bg-gradient-to-b from-[#030806] via-[#07110d] to-black">
-        {children}
+      <div
+        className="relative mx-auto h-dvh max-w-[430px] overflow-hidden"
+        style={{
+          backgroundImage: "url('/brain-bg.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        {/* Dunkler Overlay für bessere Lesbarkeit */}
+        <div className="absolute inset-0 bg-black/55" />
+
+        {/* Seiteninhalt */}
+        <div className="relative z-10 h-full">
+          {children}
+        </div>
       </div>
     </main>
   );
